e8c3c8c0c45948d700ce965ff2eae0da8310f0fa
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2021-09-24 10:34:59 +0530

    Add files via upload
    24 SEP

---
+++ JAVASCRIPT/index.js
@@ -0,0 +1,60 @@
+
+ let arr = [1, 2, 'rayne', 'coder']
+
+        // for (let index = 0; index < arr.length; index++) {
+        //     const element = arr[index];
+        //     console.log(arr[index])
+        // }
+
+        // for(let ele in arr){
+        //     console.log(arr[ele])
+        // }   
+
+        //___________________PUSH AND POP
+
+        // arr.push("ABCD");
+        // for (let index = 0; index < arr.length; index++) {
+        //     const element = arr[index];
+        //     console.log(arr[index])
+        // }
+        // console.log("POP")
+        // arr.pop();
+        // for (let index = 0; index < arr.length; index++) {
+        //     const element = arr[index];
+        //     console.log(arr[index])
+        // }
+
+        //___________________SHIFT AND UNSHIFT
+
+        // arr.unshift("data")
+        // for (let index = 0; index < arr.length; index++) {
+        //     const element = arr[index];
+        //     console.log(arr[index])
+        // }
+        // arr.shift()
+        // for (let index = 0; index < arr.length; index++) {
+        //     const element = arr[index];
+        //     console.log(arr[index])
+        // }
+
+
+        //CLASS AND OBJECTs
+
+        let person = {
+                name: "ABCD",
+                age:13,
+                phone: 9999999999
+        }
+        console.log(person.name)
+
+        function car(name,model,year) {
+            this.name=name;
+            this.model=model;
+            this.year=year;
+        }
+        var myCar = new car('o','McD',2021);
+        console.log(myCar.year)
+
+
+
+
