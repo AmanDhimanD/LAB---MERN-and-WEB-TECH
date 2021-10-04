f97aaf639f57155682902415ca7d8f8f5053ac7f
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2021-10-04 10:40:15 +0530

    Add files via upload

---
+++ WebTech Lab/string.js
@@ -0,0 +1,14 @@
+
+let s1 ='abcd'
+console.log(s1.length);
+console.log(s1.toUpperCase())
+
+console.log(s1.toLowerCase())
+
+let s2 ='efgh'
+console.log(s1.concat(s2))
+
+
+console.log(s1.toUpperCase().concat(s2));
+
+
