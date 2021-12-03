92fcd79fb2420bb2665019c87b9cc9afc011e414
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2021-12-03 22:12:21 +0530

    Add files via upload

---
+++ API Call ReactJS/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import  { Applications } from './components/Applications';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>API Calling </h1>
+      <Applications></Applications>
+    </div>
+  );
+}
+
+export default App;
