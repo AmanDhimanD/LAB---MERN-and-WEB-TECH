b1abca3930c3659f02a2bd09d620243111f8d766
AmanDhimanD <dhiman.1999.aman@gmail.com>
2022-02-16 23:03:48 +0530

    Updates

---
+++ precache-manifest.e78c172e80bba3d2f644f10a1f1d09bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f12766867a65d9563c28a878a1307b09",
+    "url": "/MyApp/index.html"
+  },
+  {
+    "revision": "b4822feef4fa4c9407b4",
+    "url": "/MyApp/static/css/2.ad278b68.chunk.css"
+  },
+  {
+    "revision": "8c8749d914de418d6062",
+    "url": "/MyApp/static/css/main.7a973784.chunk.css"
+  },
+  {
+    "revision": "b4822feef4fa4c9407b4",
+    "url": "/MyApp/static/js/2.52a85567.chunk.js"
+  },
+  {
+    "revision": "8c8749d914de418d6062",
+    "url": "/MyApp/static/js/main.73ad5fc1.chunk.js"
+  },
+  {
+    "revision": "72fcbf4678808c134083",
+    "url": "/MyApp/static/js/runtime~main.277d1b58.js"
+  },
+  {
+    "revision": "f0e5959c4a028c1634b99f3b874d210e",
+    "url": "/MyApp/static/media/logo.f0e5959c.png"
+  }
+]);
